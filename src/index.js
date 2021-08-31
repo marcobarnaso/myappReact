51dc96e1f9efd1eff81f874b49cc3442e76a90fa
Marvin Rojas Elizondo <mrojase@snap.snaptechnology.net>
2021-08-30 20:38:58 -0600

    first commit

---
+++ src/index.js
@@ -0,0 +1,28 @@
+// import the React and ReactDOM libraries
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+// Create a React component
+
+// function getButtonText() {
+//     return 'Click on me!'
+// }
+
+const App = () => {
+    const buttonText = { text: 'Click me!'};
+    const labelText = 'Enter name:'
+    return (
+
+        <div>
+            <label className="label" htmlFor="name">{labelText}</label>
+            <input id="name" type="text" />
+            <button style={{ backgroundColor: 'blue', color: 'white' }}>{buttonText.text}</button>
+        </div>
+    );
+};
+// Take the React component and show it on the screen
+ReactDOM.render(
+    <App />,
+    document.querySelector('#root')
+);
